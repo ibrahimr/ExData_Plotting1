5655b471107e75384ccc429564134ff2f03d6f57
ibrahimr <ragia.ibrahim@gmail.com>
2016-07-04 14:12:33 +0200

    Add files via upload

--- ExDataCode.R
+++ ExDataCode.R
@@ -7,40 +7,37 @@
 # data <-   read.table ("./data/household_power_consumption.txt",header=TRUE,sep=';')
 data<-read.table("./data/household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
 
-
-#subset the  data set from Feb. 1, 2007 to Feb. 2, 2007
-dat <- subset(data,Date >= as.Date("2007-2-1") & Date <= as.Date("2007-2-2"))
+## Format date to Type Date
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
 
 
 ## Combine Date and Time column
-dateTime <- paste(dat$Date,data$Time)
+dateTime <- paste(data$Date,data$Time)
 
 ## Remove incomplete observation
-dat <- dat[complete.cases(dat),]
+data <- data[complete.cases(data),]
 
 ## Combine Date and Time column
-dat$dateTime <- paste(dat$Date, dat$Time)
+data$dateTime <- paste(data$Date, data$Time)
  
 ## Format dateTime Column
-dat$dateTime <- as.POSIXct(dateTime)
+data$dateTime <- as.POSIXct(dateTime)
 
 ## Format dateTime Column
-dat$dateTime <- as.POSIXct(dat$dateTime) #representing calendar dates and times.
+data$dateTime <- as.POSIXct(data$dateTime) #representing calendar dates and times.
 
-
-## Format date to Type Date
-dat$Date <- as.Date(dat$Date, "%d/%m/%Y")
-
+ 
 #plot 1 x= global active power, y= freq, hist
  
 #first
 png("plot1.png")
 
- hist(dat$Global_active_power  , freq=TRUE, col="red" , xlab="Global active power (kilowates)"     ,main="Global_active_power",xlim=c(0,6)    ) 
+ hist(data$Global_active_power  , freq=TRUE, col="red" , xlab="Global active power (kilowates)"     ,main="Global_active_power",xlim=c(0,6)    ) 
  dev.off()
  # plot 2
  png("plot2.png")
-
+ #subset the  data set from Feb. 1, 2007 to Feb. 2, 2007
+ dat <- subset(data,Date >= as.Date("2007-2-1") & Date <= as.Date("2007-2-2"))
  plot(dat$Global_active_power~dat$dateTime, type="l", ylab="Global Active Power (kilowatts)")
 dev.off()
 
